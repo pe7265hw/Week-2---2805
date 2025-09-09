6625bec652698f62664b2d1a515956584afd790e
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2025-09-08 20:28:22 -0500

    Finished lab, class code in author.py, creating instances and runing code in main

--- author.py
+++ author.py
@@ -1,12 +1,20 @@
+from dataclasses import dataclass
+
 class Author:
+    #Initializing the Author class, accepts name, contains a list of books published
     def __init__(self, name):
         self.name = name
         self.books_published = []
 
+    #Appends a title to the authors bibliography, does not add if the title already exists
     def publish(self, title):
-        self.books_published.append(title)
+        if title in self.books_published:
+            print(f'Sorry, {title} is already in the authors bibliography!')
+        else:
+            self.books_published.append(title)
         return self.books_published
 
+    #Prints a formatted statement of the author and their books
     def __str__(self):
         if self.books_published:
             published= ", ".join(self.books_published)
@@ -14,8 +22,21 @@ class Author:
         else:
             "This author has no books published."
     
-a1 = Author("Stephen King")
-a1.publish("Cujo")
-a1.publish("IT")
 
-print(a1)
+
+"""To be perfectly honest besides the decoration(?) of @dataclass im not exactly sure
+what seperates a dataclass from a regular class, I did not realize we were supposed to
+have watched the videos before class on monday and I did not get a chance to watch the videos.
+My mistake, I'll make sure to be ready for next class."""
+@dataclass
+class Student:
+    def __init__(self,name, school_id, gpa):
+        self.name = name
+        self.school_id = school_id
+        self.gpa = gpa
+    
+    def __str__(self):
+        return f'Student name: {self.name}, ID: {self.school_id}, current GPA = {self.gpa}'
+  
+
+    
--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import author as aut
 
-aut.Author('Stephen King')
+#Adding data to classes for both Author and Student, did not realize we would be creating another class so both are in Author file, I guess I could
+#have created a seperate one for Student but I decided not to since thise won't be a larger project.
+a1 = aut.Author("Stephen King")
+a1.publish("Cujo")
+a1.publish("IT")
+a1.publish("IT")
+
+print(a1)
+
+student1 = aut.Student('Johnny', 7, 3.5)
+student2 = aut.Student('Sally Mae', 8, 3.1)
+student3 = aut.Student('Johnny Goodiegood', 2, 4.0)
+
+print(student1)
